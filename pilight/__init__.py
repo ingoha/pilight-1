7db95ad06ad4fd0386c5eaff81158e4a31014ceb
Ingo Haschler <ihaschler@gmail.com>
2026-06-04 13:24:45 +0200

    Fix for setuptools>=82.0.0

--- pilight/__init__.py
+++ pilight/__init__.py
@@ -1,12 +1,18 @@
 # http://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
-from pkg_resources import get_distribution, DistributionNotFound
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib.metadata import version, PackageNotFoundError
+else:
+    # Fallback für uralte Python-Versionen (< 3.8) via external package
+    from importlib_metadata import version, PackageNotFoundError
 
 __project__ = 'pilight'
 __version__ = None  # required for initial installation
 
 try:
-    __version__ = get_distribution(__project__).version
-except DistributionNotFound:
+    __version__ = version(__project__)
+except PackageNotFoundError:
     VERSION = __project__ + '-' + '(local)'
 else:
     VERSION = __project__ + '-' + __version__
